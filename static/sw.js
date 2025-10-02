2f3482c5c8dd251c4f2591d59c9509b6bae5f40c
Ayush Patil <aayushpatilofficial@gmail.com>
2025-10-02 18:05:22 +0530

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'vnetwork-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/sw.js',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png',
+  // Add any other static files you want to cache
+];
+
+// Install event: caching app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate event: clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event: serve cached files if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
